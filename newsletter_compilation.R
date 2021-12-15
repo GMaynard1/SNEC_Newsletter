031d88431e191348858e55a69f142dc0e0c06438
George Maynard <george.maynard@noaa.gov>
2021-12-15 13:12:32 -0500

    added production email and added Andrew to test email

--- newsletter_compilation.R
+++ newsletter_compilation.R
@@ -2,19 +2,28 @@
 library(blastula)
 library(keyring)
 ## Enter email credentials if you haven't already
-blastula::create_smtp_creds_key(
-  id="snec_gmail",
-  user="snec.newsletter@gmail.com",
-  provider="gmail"
-)
+# blastula::create_smtp_creds_key(
+#   id="snec_gmail",
+#   user="snec.newsletter@gmail.com",
+#   provider="gmail"
+# )
 ## Prep the newsletter message from the markdown file
 newsletter=blastula::render_email(
   input=file.choose()
 )
+## Test
 blastula::smtp_send(
   email=newsletter,
-  to="george.maynard@noaa.gov",
+  to=c("george.maynard@noaa.gov","andrew.bade@ct.gov"),
   from="snec.newsletter@gmail.com",
-  subject="Newsletter Test",
+  subject="TEST Newsletter",
   credentials=creds_key(id = "snec_gmail")
 )
+# ## Production
+# blastula::smtp_send(
+#   email=newsletter,
+#   to="AFS_SNEC-L@LISTSERV.UCONN.EDU",
+#   from="snec.newsletter@gmail.com",
+#   subject="SNEC Newsletter",
+#   credentials=creds_key(id = "snec_gmail")
+# )
