f950bfcab07e029232d994b31957ca1985e276a1
George Maynard <george.maynard@noaa.gov>
2022-04-25 10:42:15 -0400

    published

--- newsletter_compilation.R
+++ newsletter_compilation.R
@@ -1,6 +1,5 @@
 ## Call the necessary packages
 library(blastula)
-library(keyring)
 ## Enter email credentials if you haven't already
 # blastula::create_smtp_creds_key(
 #   id="snec_gmail",
@@ -14,16 +13,26 @@ newsletter=blastula::render_email(
 ## Test
 blastula::smtp_send(
   email=newsletter,
-  to=c("george.maynard@noaa.gov","andrew.bade@ct.gov"),
+  # to=c(
+  #   "george.maynard@noaa.gov",
+  #   "aarcher@barnstablecounty.org",
+  #   "andrew.bade@ct.gov",
+  #   "christopher.mcdowell@ct.gov"
+  #   ),
+  to="george.maynard@noaa.gov",
   from="snec.newsletter@gmail.com",
   subject="TEST Newsletter",
-  credentials=creds_key(id = "snec_gmail")
+  credentials=creds_key(
+    id='SNEC_MailR'
+  )
 )
 ## Production
 blastula::smtp_send(
   email=newsletter,
-  to="AFS_SNEC-L@LISTSERV.UCONN.EDU",
+  to="eric.schultz@uconn.edu",
   from="snec.newsletter@gmail.com",
   subject="SNEC Newsletter",
-  credentials=creds_key(id = "snec_gmail")
+  credentials=creds_key(
+    id='SNEC_MailR'
+  )
 )
